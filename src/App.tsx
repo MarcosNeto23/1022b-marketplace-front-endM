25704769cf92f22217a6f4fb1f58ece944200f42
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-10-23 06:40:29 -0400

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+// Definindo o tipo para Produto
 type ProdutoType = {
   id:number,
   nome:string,
@@ -8,28 +9,63 @@ type ProdutoType = {
   preco:string,
   imagem:string
 }
-function App() {
-  const [produtos,setProdutos] = useState<ProdutoType[]>([])
 
+// Definindo o tipo para Usuário
+type UsuarioType = {
+  id: number;
+  nome: string;
+  email: string;
+};
+
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+
+  //useEffect(O QUe fazer, Quando Fazer)
+   // useEffect para buscar produtos
   useEffect(()=>{
-    fetch("https://one022b-marketplace-m.onrender.com/produtos")
+    fetch("https://one022b-marketplace-fork-back-end.onrender.com/produtos")
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
   },[])
+
+
+  // useEffect para buscar usuários
+  useEffect(() => {
+    fetch("https://one022b-marketplace-fork-back-end.onrender.com/usuarios") // Altere para a URL correta do seu backend
+      .then(resposta => resposta.json())
+      .then(dados => setUsuarios(dados));
+  }, []);
+
+
+
   return (
-    <>
-    <div className="container-produtos">
-      {produtos.map(prod=>{
-        return(
-          <div key={prod.id} className="produto-item">
-            <h1>{prod.nome}</h1>
-            <p>{prod.descricao}</p>
-            <p>{prod.preco}</p>
-            <img src={prod.imagem} alt="Imagem de Celular" />
-          </div>
-        )
-      })}
+    <>  
+      <div className="container-produtos">
+        <h2> Lista de Produtos:</h2>
+        {produtos.map(prod=>{
+          return(
+            <div key={prod.id} className="produto-item">
+              <h1>{prod.nome}</h1>
+              <img src={prod.imagem} alt="Imagem de celular" />
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div>
+            
+          )
+        })}
       </div>
+
+      <div className="container-usuarios">
+        <h2>Lista de Usuários:</h2>
+        {usuarios.map(usuario => (
+          <div key={usuario.id} className="usuario-item">
+            <p><strong>Nome:</strong> {usuario.nome}</p>
+            <p><strong>Email:</strong> {usuario.email}</p>
+          </div>
+        ))}
+      </div>
+
     </>
   )
 }
