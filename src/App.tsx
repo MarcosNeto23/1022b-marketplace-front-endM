b0c48d640a7e7d564d1d82e5bb03849e64a44aa3
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-11-19 07:32:25 -0400

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-// Definindo o tipo para Produto
 type ProdutoType = {
   id:number,
   nome:string,
@@ -9,43 +8,17 @@ type ProdutoType = {
   preco:string,
   imagem:string
 }
-
-// Definindo o tipo para Usuário
-type UsuarioType = {
-  id: number;
-  nome: string;
-  email: string;
-};
-
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
-  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
-
   //useEffect(O QUe fazer, Quando Fazer)
-   // useEffect para buscar produtos
   useEffect(()=>{
     fetch("http://localhost:8000/produtos")
     .then(resposta=>resposta.json())
     .then(dados=>setProdutos(dados))
   },[])
-
-
-  // useEffect para buscar usuários
-  useEffect(() => {
-    fetch("https://localhost:8000/usuarios") // Altere para a URL correta do seu backend
-      .then(resposta => resposta.json())
-      .then(dados => setUsuarios(dados));
-  }, []);
-
-
-
   return (
-    <> 
-    <div className='tudo'>
-
-
+    <>  
       <div className="container-produtos">
-        <h2> Lista de Produtos:</h2>
         {produtos.map(prod=>{
           return(
             <div key={prod.id} className="produto-item">
@@ -54,24 +27,10 @@ function App() {
               <p>{prod.preco}</p>
               <p>{prod.descricao}</p>
             </div>
-            
           )
         })}
       </div>
-
-      <div className="container-usuarios">
-        <h2>Lista de Usuários:</h2>
-        {usuarios.map(usuario => (
-          <div key={usuario.id} className="usuario-item">
-            <p><strong>Nome:</strong> {usuario.nome}</p>
-            <p><strong>Email:</strong> {usuario.email}</p>
-          </div>
-        ))}
-      </div>
-
-
       
-      </div> 
     </>
   )
 }
